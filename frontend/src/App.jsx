6fd534e4f843ae48f3d58b44791338610fdc53d4
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-25 13:57:20 +0530

    install package axios for fetching api and useeffect for rendering the data once page render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [jokes, setjokes] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/jokes')
+      .then((response) => {
+        setjokes(response.data)
+      })
+      .catch(() => {
+        console.log(error)
+      })
+  })
   return (
     <>
-     <h1>FullStack Testing </h1>
+      <h1>FullStack Testing </h1>
+      <p>JOKES: {jokes.length}</p>
+      {
+        jokes.map((joke, index) => {
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        })
+      }
     </>
   )
 }
