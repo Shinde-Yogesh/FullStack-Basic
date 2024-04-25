b15d3fd96c8edda810c4cf09f6937f7bdb9d5755
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-25 13:20:24 +0530

    creating simple api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,43 @@
 import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Server is ready")
+// app.get('/', (req, res) => {
+//     res.send("Server is ready")
+// })
+
+// get the list of 5 jokes
+app.get('/jokes',(req,res) =>{
+    const jokes = [
+        {
+            id:1,
+            title:'A Joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Another Joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'third Joke',
+            content:'This is third joke'
+        },
+        {
+            id:4,
+            title:'four Joke',
+            content:'This is fout joke'
+        },
+        {
+            id:5,
+            title:'five Joke',
+            content:'This is five joke'
+        },
+    ]
+    res.send(jokes)
 })
+
+
 
 const port = process.env.PORT || 3000;
 
