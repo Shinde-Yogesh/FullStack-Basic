92da23cfc994f0a6ac13fa7dc613488c9dab3129
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-25 17:06:13 +0530

    the return keyword is important

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,13 +19,16 @@ function App() {
     <>
       <h1>FullStack Testing </h1>
       <p>JOKES: {jokes.length}</p>
+
+ {/* use parenthesis for don't want use return keyword */}
       {
-        jokes.map((joke, index) => {
+        jokes.map((joke, index) => (
           <div key={joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
-        })
+        ))
+
       }
     </>
   )
